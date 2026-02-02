cb3247ccd5a4953b4a5579f080f43fb5f815a634
S KARTHIK <skarthik.link@gmail.com>
2026-02-02 17:33:59 +0530

    feat: add exception propagation handling example

--- src/javacollectionsandstreams/exceptions/PropagationExceptionHandler.java
+++ src/javacollectionsandstreams/exceptions/PropagationExceptionHandler.java
@@ -1,4 +1,14 @@
 package javacollectionsandstreams.exceptions;
 
 public class PropagationExceptionHandler {
+    static void m1(){ int x = 10/0; }
+    static void m2(){ m1(); }
+
+    public static void main(String[] args) {
+        try {
+            m2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
 }
