b21bea21425eb8f78d168c702eed481fce280c9a
S KARTHIK <skarthik.link@gmail.com>
2026-02-02 17:33:53 +0530

    feat: implement nested exception handling example

--- src/javacollectionsandstreams/exceptions/NestedExceptionHandler.java
+++ src/javacollectionsandstreams/exceptions/NestedExceptionHandler.java
@@ -1,4 +1,17 @@
 package javacollectionsandstreams.exceptions;
 
 public class NestedExceptionHandler {
+    public static void main(String[] args) {
+        int[] a = {10,20,30};
+        try {
+            int v = a[5];
+            try {
+                System.out.println(v/0);
+            } catch (ArithmeticException e) {
+                System.out.println("Cannot divide by zero!");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid array index!");
+        }
+    }
 }
