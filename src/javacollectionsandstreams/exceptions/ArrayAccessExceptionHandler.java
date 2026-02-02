2e13ebebebea98ebbf32b684b2b69a7c68674d8f
S KARTHIK <skarthik.link@gmail.com>
2026-02-02 17:33:13 +0530

    feat: implement array access exception handler

--- src/javacollectionsandstreams/exceptions/ArrayAccessExceptionHandler.java
+++ src/javacollectionsandstreams/exceptions/ArrayAccessExceptionHandler.java
@@ -1,4 +1,14 @@
 package javacollectionsandstreams.exceptions;
 
 public class ArrayAccessExceptionHandler {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30};
+        try {
+            System.out.println(arr[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+    }
 }
