a7f35ce0e8bd5c96ad8bae553664460168298f53
S KARTHIK <skarthik.link@gmail.com>
2026-02-02 17:34:05 +0530

    feat: implement resource read exception handler

--- src/javacollectionsandstreams/exceptions/ResourceReadExceptionHandler.java
+++ src/javacollectionsandstreams/exceptions/ResourceReadExceptionHandler.java
@@ -1,4 +1,13 @@
 package javacollectionsandstreams.exceptions;
 
+import java.io.*;
+
 public class ResourceReadExceptionHandler {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
+            System.out.println(br.readLine());
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
 }
