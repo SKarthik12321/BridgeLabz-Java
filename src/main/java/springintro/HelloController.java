11c037b310788a45813af4d4039736c0ee7a8fee
S KARTHIK <skarthik.link@gmail.com>
2026-03-11 12:16:42 +0530

    feat(uc2): add hello API using query parameter

--- src/main/java/springintro/HelloController.java
+++ src/main/java/springintro/HelloController.java
@@ -1,13 +1,22 @@
 package springintro;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    // UC1
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from Spring Boot!";
+    public String hello() {
+        return "Hello from BridgeLabz";
     }
+
+    // UC2
+    @GetMapping("/hello/query")
+    public String helloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
 }
